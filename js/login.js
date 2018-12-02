60a27b64343d8a99fe26229712906adedb4a8c01
artedom <caesar.cluster@gmail.com>
2018-12-02 00:13:46 -0200

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,10 @@
+function Login(){
+var done=0;
+var username=document.login.username.value;
+username=username.toLowerCase();
+var password=document.login.password.value;
+password=password.toLowerCase();
+if (username=="dom" && password=="artedom") { window.location="https://artedom.github.io/menu.html"; done=1; }
+//if (username=="outro" && password=="outrasenha") { window.location="http://www.seu_site.com.br"; done=1; }
+if (done==0) { window.location="https://artedom.github.io/links/novamente.html"; done=1; }
+}

73531a09cdefb37a1586914e9d58db4d2bd109af
artedom <caesar.cluster@gmail.com>
2018-12-02 00:24:11 -0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -6,5 +6,5 @@ var password=document.login.password.value;
 password=password.toLowerCase();
 if (username=="dom" && password=="artedom") { window.location="https://artedom.github.io/menu.html"; done=1; }
 //if (username=="outro" && password=="outrasenha") { window.location="http://www.seu_site.com.br"; done=1; }
-if (done==0) { window.location="https://artedom.github.io/links/novamente.html"; done=1; }
+if (done==0) { window.location="https://artedom.github.io/links/login1.html"; done=1; }
 }
